38ece7e0f717f48d06bd7aec9f15b3dabd8f7384
Alexander Mendelsohn <superparkourio@gmail.com>
2018-03-08 16:38:14 -0500

    Damage and DamageDisplay added.
    Damage currently stores a float and nothing more. The Player1 and Player2 GameObjects have Damage components attached.
    
    DamageDisplay shows these numbers. It has a damages array, an array of Damage components. If one of this array's values is null, it assumes that player doesn't exist. The DamageGUI array holds the actual Text components to show the numbers. There can be no null values in this array. I've created a GUICanvas GameObject and attached it to the Main Camera GameObject. This GUICanvas object has the Damage Display component attached.

---
+++ Assets/Damage.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This component keeps track of the amount of damage accumulated.
+/// </summary>
+public class Damage : MonoBehaviour {
+
+	public float damage; // the amount of damage accumulated
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+}
